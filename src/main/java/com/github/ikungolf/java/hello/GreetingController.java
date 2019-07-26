ed176ed62e26884dbfb19e3c53eabdda6e9a3fac
Theerachai Songsee <golfsoccer@gmail.com>
2019-07-26 11:07:33 +0700

    modify GreetingController adding response content-type

--- src/main/java/com/github/ikungolf/java/hello/GreetingController.java
+++ src/main/java/com/github/ikungolf/java/hello/GreetingController.java
@@ -1,8 +1,11 @@
 package com.github.ikungolf.java.hello;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +14,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @RequestMapping(value="/greeting", method=RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public @ResponseBody Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
